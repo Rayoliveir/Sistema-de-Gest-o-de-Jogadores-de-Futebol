9d32b3bdb4749542325c3c2bf7c9ee445c893aaa
Marcelly Oliveira <160605874+Rayoliveir@users.noreply.github.com>
2025-05-26 10:26:01 -0300

    Delete api directory

--- api/src/main/java/com/example/aula/exception/NumeroDaCamisaJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.aula.exception;
-
-public class NumeroDaCamisaJaCadastradoException extends RuntimeException {
-    public NumeroDaCamisaJaCadastradoException(String message) {
-        super(message);
-    }
-}
